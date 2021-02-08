c4059a3d43383d221fc9da8493ce9e9abfea9a40
cjbeattie <courtney.j.beattie+1@gmail.com>
2021-02-08 11:23:47 +0800

    Adding portflio table, add button

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,5 +1,14 @@
+import EnhancedTable from './EnhancedTable'
+import AddToPortfolioButton from './AddToPortfolioButton'
+
 const Portfolio = () => {
-    return <h1>Portfolio</h1>
+    return (
+        <>
+            <h1>Portfolio</h1>
+            <AddToPortfolioButton />
+            <EnhancedTable />
+        </>
+    )
 }
 
 export default Portfolio;
