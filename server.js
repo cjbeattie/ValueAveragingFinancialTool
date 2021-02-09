5c590c8a7ec9c72359bfa97ce0402f061dd1f546
cjbeattie <courtney.j.beattie+1@gmail.com>
2021-02-09 11:49:11 +0800

    Adding script for nodemon

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
 });
